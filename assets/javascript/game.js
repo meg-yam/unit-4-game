c74fd46c58dfd48bf880b4c1693d4cb42fa920ee
meg-yam <megyam95@gmail.com>
2019-04-02 22:23:11 -0400

    Finished - Errors Remain

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+var targetNumber = 55;
+$("#randomNum").text(targetNumber);
+
+var counter = 0;
+
+var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var increment = numberOptions[Math.round(Math.random())];
+
+for (var i = 0; i < numberOptions.length; i++) {
+    var imageCrystal = $("<img>");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("src", "../images/Blue.jpeg");
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+    $("#crystals").append(imageCrystal);
+}
+
+
+$(".image").on("click", function() {
+   
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalVaue = parseInt(crystalValue);
+   
+    counter += crystalValue;
+
+    $("#score").text(counter);
+
+    if (counter === targetNumber) {
+        $("#alarm").text("You win!");
+    } 
+    else if (counter >= targetNumber) {
+        $("#alarm").text("You lose!");
+    }
+});
